c020fc2d7b7a886950ea0c8d1a2d3b169ef90e45
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-01-17 18:07:18 +0000

    inital set of state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import './App.css';
+import React, { Component } from "react";
+import './index.css';
 
-function App() {
 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      general: [ 
+        {name: 'Aaron'},
+        {email: '@'},
+        {phone: '0'}
+      ],
+      education : [],
+      experience : [],
+    };
+  }
+
+  render() {
+    const general = this.state.general[0].name
+
+    return (
+      <div>
+        <p>{general}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
