21446e419f9b0fe5952611b788c3ef7bba064e97
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-01-26 15:05:28 +0000

    small changes

---
+++ src/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+
+  render(){
+    return(
+      <>
+        <div className = 'header'>
+          <h1>CV Creator</h1>
+        </div>
+      </>
+    )
+  }
+}
