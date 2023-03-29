2230be8b4b9c29c565fdcc719af5427a2902e919
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-03-29 12:11:08 +0100

    change how skills load on PDF, change how skills load onto 'ALL'

--- src/showSkills.js
+++ src/showSkills.js
@@ -8,26 +8,40 @@ export const ShowSkills = props => {
   // changed how its called depending on parameters
   // if no skills, dont load its heading
   return (
-      <div>
-        { 
-        skills.length !== 0 ?
+    <div>
+      {skills.length !== 0 ? (
         <div className="eachCategory">
           <h3>Skills</h3>
-          <ul className="holder skills">
-            {skills.map((skill) => {
-              return (
-              skill.skill !== '' &&
-              <li key={skill.key}>{skill.skill}<Icon className="bin" path={mdiDelete} size={.90} onClick = {() => props.delete('skills', skill.key)}/></li>
-              )
-            })}
+          <ul className="holder">
+            <div className="Info skills">
+              {skills.map((skill) => {
+                return (
+                  skill.skill !== "" && (
+                    <li key={skill.key}>
+                      {skill.skill}
+                      <Icon
+                        className="bin"
+                        path={mdiDelete}
+                        size={0.9}
+                        onClick={() => props.delete("skills", skill.key)}
+                      />
+                    </li>
+                  )
+                );
+              })}
+            </div>
           </ul>
-          <button className="add" onClick = {props.handleAddSkill}>Add another skill</button>
+          <button className="add" onClick={props.handleAddSkill}>
+            Add another skill
+          </button>
         </div>
-        :
+      ) : (
         <div className="eachCategory">
-          <button className="add" onClick = {props.handleAddSkill}>Add a skill</button>
+          <button className="add" onClick={props.handleAddSkill}>
+            Add a skill
+          </button>
         </div>
-        }
-      </div>
-  )
+      )}
+    </div>
+  );
 }
