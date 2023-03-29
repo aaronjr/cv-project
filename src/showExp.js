5e22ba6bcd965dfd6b4e1ac4f4257f4348db61d4
Aaron Richards <aaron_richards@hotmail.co.uk>
2023-03-29 12:02:55 +0100

    remove most media queries using clamp for width instead

--- src/showExp.js
+++ src/showExp.js
@@ -9,14 +9,37 @@ export const ShowExp = props => {
       <h3>Experience</h3>
         {experience.sort((a, b) => b.yearTo - a.yearTo).map((exp) => {
           return (
-          exp.employer !== '' &&
-          <ul className = "holder" key={exp.key}>
-            <li className="titleInfo">{exp.employer} <span className="dates">{exp.yearFrom} - {exp.yearTo}</span></li>
-            <li style={{fontStyle:'italic', marginBottom:'5px'}}>{exp.title}</li>
-            <li className="bio">{exp.description}</li>
-            <button className="edit" onClick = {() => props.onClick(exp.key)}>Edit</button>
-            <button className="del" onClick = {() => props.delete('experience', exp.key)}>Delete</button>
-          </ul>)
+            exp.employer !== "" && (
+              <ul className="holder" key={exp.key}>
+                <div className="Info">
+                  <li className="titleInfo">
+                    {exp.employer}{" "}
+                    <span className="dates">
+                      {exp.yearFrom} - {exp.yearTo}
+                    </span>
+                  </li>
+                  <li style={{ fontStyle: "italic", marginBottom: "5px" }}>
+                    {exp.title}
+                  </li>
+                  <li className="bio">{exp.description}</li>
+                </div>
+                <div className="buttonHolder">
+                  <button
+                    className="edit"
+                    onClick={() => props.onClick(exp.key)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="del"
+                    onClick={() => props.delete("experience", exp.key)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </ul>
+            )
+          );
         })}
         <button className="add" onClick = {() => props.onClick(0)}>Add an experience</button>
     </div>
